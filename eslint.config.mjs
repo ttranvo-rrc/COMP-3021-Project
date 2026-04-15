b2ef0bb15a5eec1ac3f2e810ad70b8e70a56e92e
Tri Tranvo <ttranvo@academic.rrc.ca>
2026-04-15 15:48:39 -0500

    installed some packages, updated eslint config again.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,43 @@
 import js from "@eslint/js";
 import security from "eslint-plugin-security";
+import globals from "globals";
 
 export default [
+  {
+    ignores: [
+      "bundle.js",
+      "node_modules/**"
+    ]
+  },
+
   js.configs.recommended,
+
   {
     files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      sourceType: "commonjs"
+    },
     plugins: {
       security
     },
     rules: {
       ...security.configs.recommended.rules
+    }
+  },
+
+  {
+    files: ["**/*.test.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        ...globals.browser
+      },
+      sourceType: "commonjs"
     }
   }
 ];
